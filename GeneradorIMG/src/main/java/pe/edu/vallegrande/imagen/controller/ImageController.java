1602d880419dddf5e2b9eb7ef5ac3236653f2edc
Luis Tasayco <127450834+LuisTasaycoQuispe@users.noreply.github.com>
2024-10-11 10:15:45 +0000

    Kubernetes

--- GeneradorIMG/src/main/java/pe/edu/vallegrande/imagen/controller/ImageController.java
+++ GeneradorIMG/src/main/java/pe/edu/vallegrande/imagen/controller/ImageController.java
@@ -8,10 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import pe.edu.vallegrande.imagen.service.ImageGenerationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/generadorIMG")
 public class ImageController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ImageController.class); // Declaración del logger
 
     private final ImageGenerationService imageGenerationService;
 
@@ -22,14 +26,17 @@ public class ImageController {
     @PostMapping("/imagen3d")
     public ResponseEntity<?> generateImage3D(@Valid @RequestBody ImageRequest imageRequest) {
         try {
+            logger.info("Generando una imagen 3D con la descripcion: {}", imageRequest.getInput());
+
             String response = imageGenerationService.generateImage3D(imageRequest.getInput());
+
             return ResponseEntity.ok(response);
         } catch (Exception e) {
+            logger.error("Error al generar la imagen 3D : {}", e.getMessage()); 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error generating 3D image: " + e.getMessage());
         }
     }
-
 }
 
 class ImageRequest {
